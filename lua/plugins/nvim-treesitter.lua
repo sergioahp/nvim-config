3f1e0e1ea99de1a38b2f5c64e84eb01868e11544
sergioahp <sergioahp@proton.me>
2025-02-14 03:08:20 -0600

    move telescope options to opts instead of config

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -3,17 +3,15 @@ local M = {
   build = function()
     require("nvim-treesitter.install").update({ with_sync = true })()
   end,
-  config = function ()
-    local configs = require("nvim-treesitter.configs")
-
-    configs.setup({
-      ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'html' , 'typst', 'bash', 'luadoc', 'markdown', 'requirements', 'toml', 'yaml', 'python', 'nix', },
-      sync_install = false,
-      highlight = { enable = true, additional_vim_regex_highlighting = false },
-      indent = { enable = true },
-    })
+  opts = {
+    ensure_installed = { 'c', 'lua', 'vim', 'vimdoc', 'query', 'javascript', 'html' , 'typst', 'bash', 'luadoc', 'markdown', 'requirements', 'toml', 'yaml', 'python', 'nix', 'yuck' },
+    sync_install = false,
+    highlight = { enable = true, additional_vim_regex_highlighting = false },
+    indent = { enable = true },
+  },
+  config = function (_, opts)
+    require("nvim-treesitter.configs").setup(opts)
     vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
-
   end
 }
 
