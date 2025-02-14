2e0609e9fad93457b1e81c87013ed008ac8b78d0
sergioahp <sergioahp@proton.me>
2025-02-14 03:31:44 -0600

    add treesitter text objects

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,5 +1,8 @@
 local M = {
   "nvim-treesitter/nvim-treesitter",
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter-textobjects'
+  },
   build = function()
     require("nvim-treesitter.install").update({ with_sync = true })()
   end,
@@ -15,6 +18,69 @@ local M = {
         node_incremental = "<leader>i",
         scope_incremental = "<leader>ts",
         node_decremental = "<leader>I",
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+
+        lookahead = true,
+        lookbehind = true,
+
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['ib'] = '@block.inner',
+          ['ab'] = '@block.outer',
+          ['iC'] = '@call.inner',
+          ['aC'] = '@call.outer',
+          ['ic'] = '@class.inner',
+          ['ac'] = '@class.outer',
+          ['ak'] = '@comment.outer',
+          ['id'] = '@conditional.inner',
+          ['ad'] = '@conditional.outer',
+          ['ir'] = '@frame.inner',
+          ['ar'] = '@frame.outer',
+          ['if'] = '@function.inner',
+          ['af'] = '@function.outer',
+          ['iL'] = '@loop.inner',
+          ['aL'] = '@loop.outer',
+          ['iP'] = '@parameter.inner',
+          ['aP'] = '@parameter.outer',
+          ['iK'] = '@scopename.inner',
+          ['aK'] = '@scopename.outer',
+          ['iM'] = '@statement.innet',
+          ['aM'] = '@statement.outer',
+        },
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = false, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[k'] = '@block.outer',
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },
       },
     },
   },
