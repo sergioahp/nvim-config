7b5b2795722dae6e577971466fe0afd6df66f36c
sergioahp <sergioahp@proton.me>
2025-02-14 03:34:40 -0600

    add ui-select with telescope

--- lua/plugins/nvim-telescope.lua
+++ lua/plugins/nvim-telescope.lua
@@ -3,15 +3,21 @@ return {
   event = 'VeryLazy',
   dependencies = {
     'nvim-lua/plenary.nvim',
+    'nvim-telescope/telescope-ui-select.nvim',
     { 'nvim-telescope/telescope-fzf-native.nvim', build ='make' },
   },
   config = function ()
     require('telescope').setup {
       extensions = {
         fzf = {},
+        ['ui-select'] = {
+          require('telescope.themes').get_dropdown {
+          }
+        },
       },
     }
     require('telescope').load_extension('fzf')
+    require('telescope').load_extension('ui-select')
   end,
   keys = {
     {
