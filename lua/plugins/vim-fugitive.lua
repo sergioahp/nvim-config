c723acd173882fb5f01b6f50aa87b753c7679593
sergioahp <sergioahp@proton.me>
2025-02-18 15:49:20 -0600

    add fugitive maps to get from bufs 2 and 3

--- lua/plugins/vim-fugitive.lua
+++ lua/plugins/vim-fugitive.lua
@@ -1,4 +1,18 @@
 return {
   'tpope/vim-fugitive',
   event = 'BufEnter',
+  keys = {
+    {
+      "<leader>gk",
+      [[:diffget //2<CR>]],
+      "n",
+      desc = "diffget from 2",
+    },
+    {
+      "<leader>gj",
+      [[:diffget //3<CR>]],
+      "n",
+      desc = "diffget from 3",
+    },
+  },
 }
