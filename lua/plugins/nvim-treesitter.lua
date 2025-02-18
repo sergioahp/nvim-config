4517a472e5193d2fa4427f24bd9074acaa44e9b2
sergioahp <sergioahp@proton.me>
2025-02-18 15:28:48 -0600

    add textobjects and their motions

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -1,7 +1,85 @@
+-- Define our textobject definitions.
+-- Paired ones have both an inner and an outer capture.
+local textobj_pairs = {
+  t = { inner = "@attribute.inner",   outer = "@attribute.outer"   },
+  b = { inner = "@block.inner",       outer = "@block.outer"       },
+  c = { inner = "@call.inner",        outer = "@call.outer"        },
+  k = { inner = "@class.inner",       outer = "@class.outer"       },
+  m = { inner = "@comment.inner",     outer = "@comment.outer"     },
+  o = { inner = "@conditional.inner", outer = "@conditional.outer" },
+  r = { inner = "@frame.inner",       outer = "@frame.outer"       },
+  f = { inner = "@function.inner",    outer = "@function.outer"    },
+  l = { inner = "@loop.inner",        outer = "@loop.outer"        },
+  a = { inner = "@parameter.inner",   outer = "@parameter.outer"   },
+}
+
+-- Non-paired textobjects.
+-- Here the original select mappings used more than one character.
+local nonpaired = {
+  ee = "@assignment.inner",
+  el = "@assignment.lhs",
+  eh = "@assignment.rhs",
+  u  = "@nubmer.inner",  -- note: "nubmer" appears as in your config.
+}
+
+-- Build the select keymaps table dynamically.
+-- For paired ones, we generate an inner key "i<letter>" and an outer key "a<letter>".
+-- For non-paired ones, we generate both a lower-case version and an uppercase version.
+local select_keymaps = {}
+for letter, obj in pairs(textobj_pairs) do
+  select_keymaps["i" .. letter] = obj.inner
+  select_keymaps["a" .. letter] = obj.outer
+end
+for key, capture in pairs(nonpaired) do
+  select_keymaps["i" .. key] = capture
+  select_keymaps["I" .. string.upper(key)] = capture
+end
+
+-- Build swap mapping tables.
+local swap_next     = {}
+local swap_previous = {}
+for letter, obj in pairs(textobj_pairs) do
+  -- For paired textobjects, add both inner and outer swaps.
+  swap_next["<leader>s" .. letter]     = obj.inner
+  swap_previous["<leader>S" .. letter] = obj.inner
+  swap_next["<leader>a" .. letter]     = obj.outer
+  swap_previous["<leader>A" .. letter] = obj.outer
+end
+for key, capture in pairs(nonpaired) do
+  swap_next["<leader>s" .. key]     = capture
+  swap_previous["<leader>S" .. key] = capture
+end
+
+-- Build move mapping tables.
+local goto_next_start = {}
+local goto_next_end   = {}
+local goto_prev_start = {}
+local goto_prev_end   = {}
+for letter, obj in pairs(textobj_pairs) do
+  -- Outer textobject moves.
+  goto_next_start["<leader>n" .. letter] = obj.outer
+  goto_prev_start["<leader>p" .. letter] = obj.outer
+  goto_next_end["<leader>n" .. string.upper(letter)] = obj.outer
+  goto_prev_end["<leader>p" .. string.upper(letter)] = obj.outer
+
+  -- Inner textobject moves.
+  goto_next_start["<leader>N" .. letter] = obj.inner
+  goto_prev_start["<leader>P" .. letter] = obj.inner
+  goto_next_end["<leader>N" .. string.upper(letter)] = obj.inner
+  goto_prev_end["<leader>P" .. string.upper(letter)] = obj.inner
+end
+for key, capture in pairs(nonpaired) do
+  goto_next_start["<leader>n" .. key] = capture
+  goto_prev_start["<leader>p" .. key] = capture
+  goto_next_end["<leader>n" .. string.upper(key)] = capture
+  goto_prev_end["<leader>p" .. string.upper(key)] = capture
+end
+
+-- Now plug everything into the Treesitter configuration.
 local M = {
   "nvim-treesitter/nvim-treesitter",
   dependencies = {
-    'nvim-treesitter/nvim-treesitter-textobjects'
+    "nvim-treesitter/nvim-treesitter-textobjects",
   },
   build = function()
     require("nvim-treesitter.install").update({ with_sync = true })()
@@ -14,80 +92,46 @@ local M = {
     incremental_selection = {
       enable = true,
       keymaps = {
-        init_selection = "<leader>i",
-        node_incremental = "<leader>i",
+        init_selection    = "<leader>i",
+        node_incremental  = "<leader>i",
         scope_incremental = "<leader>ts",
-        node_decremental = "<leader>I",
+        node_decremental  = "<leader>I",
       },
     },
     textobjects = {
       select = {
         enable = true,
-
         lookahead = true,
         lookbehind = true,
-
-        keymaps = {
-          -- You can use the capture groups defined in textobjects.scm
-          ['ib'] = '@block.inner',
-          ['ab'] = '@block.outer',
-          ['iC'] = '@call.inner',
-          ['aC'] = '@call.outer',
-          ['ic'] = '@class.inner',
-          ['ac'] = '@class.outer',
-          ['ak'] = '@comment.outer',
-          ['id'] = '@conditional.inner',
-          ['ad'] = '@conditional.outer',
-          ['ir'] = '@frame.inner',
-          ['ar'] = '@frame.outer',
-          ['if'] = '@function.inner',
-          ['af'] = '@function.outer',
-          ['iL'] = '@loop.inner',
-          ['aL'] = '@loop.outer',
-          ['iP'] = '@parameter.inner',
-          ['aP'] = '@parameter.outer',
-          ['iK'] = '@scopename.inner',
-          ['aK'] = '@scopename.outer',
-          ['iM'] = '@statement.innet',
-          ['aM'] = '@statement.outer',
-        },
+        include_surrounding_whitespace = false,
+        keymaps = select_keymaps,  -- our dynamically built select mappings
       },
       swap = {
         enable = true,
-        swap_next = {
-          ['<leader>a'] = '@parameter.inner',
-        },
-        swap_previous = {
-          ['<leader>A'] = '@parameter.inner',
-        },
+        swap_next     = swap_next,
+        swap_previous = swap_previous,
       },
       move = {
         enable = true,
-        set_jumps = false, -- whether to set jumps in the jumplist
-        goto_next_start = {
-          [']m'] = '@function.outer',
-          [']]'] = '@class.outer',
-        },
-        goto_next_end = {
-          [']M'] = '@function.outer',
-          [']['] = '@class.outer',
-        },
-        goto_previous_start = {
-          ['[k'] = '@block.outer',
-          ['[m'] = '@function.outer',
-          ['[['] = '@class.outer',
-        },
-        goto_previous_end = {
-          ['[M'] = '@function.outer',
-          ['[]'] = '@class.outer',
-        },
+        set_jumps = false,
+        goto_next_start     = goto_next_start,
+        goto_next_end       = goto_next_end,
+        goto_previous_start = goto_prev_start,
+        goto_previous_end   = goto_prev_end,
       },
     },
   },
-  config = function (_, opts)
+  config = function(_, opts)
     require("nvim-treesitter.configs").setup(opts)
     vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
-  end
+    local ts_repeat_move = require "nvim-treesitter.textobjects.repeatable_move"
+    vim.keymap.set({ "n", "x", "o" }, ";", ts_repeat_move.repeat_last_move, { silent = true } )
+    vim.keymap.set({ "n", "x", "o" }, ",", ts_repeat_move.repeat_last_move_opposite, { silent = true })
+    vim.keymap.set({ "n", "x", "o" }, "f", ts_repeat_move.builtin_f_expr, { expr = true, silent = true })
+    vim.keymap.set({ "n", "x", "o" }, "F", ts_repeat_move.builtin_F_expr, { expr = true, silent = true })
+    vim.keymap.set({ "n", "x", "o" }, "t", ts_repeat_move.builtin_t_expr, { expr = true, silent = true })
+    vim.keymap.set({ "n", "x", "o" }, "T", ts_repeat_move.builtin_T_expr, { expr = true, silent = true })
+  end,
 }
 
 return { M }
---
+++ lua/plugins/substitute-nvim.lua
@@ -0,0 +1,62 @@
+return {
+  "gbprod/substitute.nvim",
+  opts = {
+    exchange = {
+      use_esc_to_cancel = false,
+      preserve_cursor_position = true,
+    },
+  },
+  event = "BufEnter",
+  keys = {
+    {
+      "cx",
+      function()
+        require("substitute.exchange").operator()
+      end,
+      mode = "n",
+      desc = "Exchange text object",
+      noremap = true,
+      silent = true,
+    },
+    {
+      "cxx",
+      function()
+        require("substitute.exchange").line()
+      end,
+      mode = "n",
+      desc = "Exchange entire line",
+      noremap = true,
+      silent = true,
+    },
+    {
+      "X",
+      function()
+        require("substitute.exchange").visual()
+      end,
+      mode = "x",
+      desc = "Exchange visual selection",
+      noremap = true,
+      silent = true,
+    },
+    {
+      "cxc",
+      function()
+        require("substitute.exchange").cancel()
+      end,
+      mode = "n",
+      desc = "Cancel exchange operation",
+      noremap = true,
+      silent = true,
+    },
+    -- {
+    --   "cxm",
+    --   function()
+    --     require("substitute.exchange").operator({ motion = "iw" })
+    --   end,
+    --   mode = "n",
+    --   desc = "Exchange current word",
+    --   noremap = true,
+    --   silent = true,
+    -- },
+  },
+}
