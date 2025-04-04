15e363438f4464cda827ad60cbdb7442f90daff2
sergioahp <sergioahp@proton.me>
2025-04-04 01:34:15 -0600

    update dbee maps

--- lua/plugins/nvim-dbee.lua
+++ lua/plugins/nvim-dbee.lua
@@ -7,7 +7,24 @@ return {
     -- "MattiasMTS/cmp-dbee",
   },
   ft = "sql",
-  opts = {}, -- needed
+  opts = {
+    editor = {
+      -- see drawer comment.
+      window_options = {},
+      buffer_options = {},
+
+      -- directory where to store the scratchpads.
+      --directory = "path/to/scratchpad/dir",
+
+      -- mappings for the buffer
+      mappings = {
+        -- run what's currently selected on the active connection
+        { key = "<leader>x", mode = "v", action = "run_selection" },
+        -- run the whole file on the active connection
+        { key = "<leader>x", mode = "n", action = "run_file" },
+      },
+    },
+  }, -- needed
   build = function ()
     -- Install tries to automatically detect the install method.
     -- if it fails, try calling it with one of these parameters:
