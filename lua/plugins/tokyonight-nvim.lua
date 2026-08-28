76491a5362a9923a0ae202afd86852864ebf26c6
sergioahp <sergioahp@proton.me>
2026-08-28 02:04:59 -0600

    Swap undercurls for underlines

--- lua/plugins/tokyonight-nvim.lua
+++ lua/plugins/tokyonight-nvim.lua
@@ -4,6 +4,13 @@ return {
   priority = 1000,
   opts = {
     transparent = true,
+    on_highlights = function(hl)
+      for _, severity in ipairs({"Error", "Warn", "Info", "Hint"}) do
+        local group = "DiagnosticUnderline" .. severity
+        hl[group].undercurl = false
+        hl[group].underline = true
+      end
+    end,
   },
   config = function (_, opts)
     require("tokyonight").setup(opts)
